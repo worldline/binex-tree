b16ffbfab5434dc110f3242db38b48c47e26817b
Damien Simonin Feugas <damien.feugas@worldline.com>
2015-08-20 15:20:39 +0200

    Add some tests on Targeting-Engine specific widget

--- request-widget/src/app.js
+++ request-widget/src/app.js
@@ -8,18 +8,25 @@ import TargetingEngineTree from './targeting-engine/tree';
 >>>>>>> f7b2f99... Working implementation of node result fetch on targeting-engine API
 
 //let request = '(mkt_sgm[value = "gold + long"] && mkt_sgm[value = "gold"] && mkt_sgm[value = "gold"] && mkt_sgm[value = "gold"] && mkt_sgm[value = "gold"] && mkt_sgm[value = "gold"] && mkt_sgm[value = "gold"] && (bought_solar[value = false] || gender[value = "male"] || gender[value = "male"] || gender[value = "male"] || gender[value = "male"] || gender[value = "male"] || gender[value = "male"] || gender[value = "male"] || gender[value = "male"] || gender[value = "male"] || gender[value = "male"] || gender[value = "male"] || gender[value = "male"] || gender[value = "male"] || gender[value = "male"] || gender[value = "male"] || gender[value = "male"] || gender[value = "male"] || gender[value = "male"]))  || (bought_solar[value = false] && gender[value = "male"])';
-let request = 'mkt_sgm [value = "gold + long"] && mkt_sgm [value = "gold"] && (bought_solar [value = true] || f1 [value = 1] || f2 [value = 2]) || bought_solar [value = false] && gender [value = "female"]';
+let request = 'sgm_esso [value >= 125] || sgm_kaufhof [value >= 15] && sgm_obi [value >= 10]';
 let widget = new TargetingEngineTree('#main', request, {targetingEngine: {base: 'client1'}});
 let error = d3.select('#error');
 let input = d3.select('#request');
+let timeout = null;
 
 function onRequestChange() {
-  error.classed('active', false);
-  try {
-    widget.setData(input.property('value'));
-  } catch (err) {
-    error.classed('active', true).html(`<span>${err}</span>`);
+  if (timeout) {
+    clearTimeout(timeout);
   }
+  timeout = setTimeout(() => {
+    timeout = null;
+    error.classed('active', false);
+    try {
+      widget.setData(input.property('value'));
+    } catch (err) {
+      error.classed('active', true).html(`<span>${err}</span>`);
+    }
+  }, 200);
 }
 
 input.property('value', request)
